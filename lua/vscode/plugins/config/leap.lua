396d8513bf52cde7159769fec6fe045a392da0e8
Luckas <luckasranarison@gmail.com>
2023-02-26 07:33:14 +0300

    add: leap config

---
+++ lua/vscode/plugins/config/leap.lua
@@ -0,0 +1,4 @@
+--Highlightings
+vim.api.nvim_set_hl(0, "LeapMatch", { fg = "#555f77", bg = "#E5E9F0" })
+vim.api.nvim_set_hl(0, "LeapLabelPrimary", { fg = "#E5E9F0", bg = "#555f77" })
+vim.api.nvim_set_hl(0, "LeapLabelSecondary", { fg = "#555f77", bg = "#88c0d0" })
