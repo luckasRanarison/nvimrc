19a7c8e2290d19ddcec0cae537084926f91c0f5b
Luckas <luckasranarison@gmail.com>
2023-02-27 18:31:00 +0300

    add: keymaps

--- lua/vscode/core/mappings.lua
+++ lua/vscode/core/mappings.lua
@@ -2,12 +2,18 @@ local mappings = {
   -- Normal mode
   n = {
     ["<leader>p"] = "\"+p", -- paste from system clipboard
+    ["<leader>P"] = "\"+P", -- on the top
     ["<leader>s"] = ":nohlsearch<cr>", -- Clear seach highlighting
     ["<leader>r"] = ":registers<cr>", -- Show registers
     ["<leader>j"] = ":jumps<cr>", -- Show jumps
-    ["<leader>é"] = "g~l", -- Toggle letter casing
+
+    -- Vscode actions
     ["<leader>c"] = "<cmd>call VSCodeNotify('editor.action.commentLine')<cr>", -- Comment line
+    ["<leader>f"] = "<cmd>call VSCodeNotify('petsView.focus')<cr>", -- Focus pets
+    ["<leader>o"] = "<cmd>call VSCodeNotify('outline.focus')<cr>", -- Focus outline
+    ["<leader>n"] = "<cmd>call VSCodeNotify('notification.clear')<cr>", -- Focus outline
     ["<leader>t"] = "<cmd>call VSCodeNotify('workbench.action.togglePanel')<cr>", -- Toggle panel
+    ["<leader>T"] = "<cmd>call VSCodeNotify('timeline.focus')<cr>", -- Focus timeline
 
     -- Quick replace mappings
     ["<leader>i"] = "ciw",
@@ -24,6 +30,7 @@ local mappings = {
   v = {
     ["<leader>y"] = "\"+y", -- Yank to system clipboard
     ["<leader>p"] = "\"+p", -- Paste from system clipboard
+    ["<leader>P"] = "\"+P", -- on the top
   },
 }
 
