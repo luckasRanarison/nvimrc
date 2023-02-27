45a088ff34c29a1618d9d69590d427f0bc7cab03
Luckas <luckasranarison@gmail.com>
2023-02-27 18:19:47 +0300

    update: made packer more flexible

--- lua/utils/configurer.lua
+++ lua/utils/configurer.lua
@@ -1,11 +1,11 @@
 local M = {}
 
----@class options
----@field opt table<string, string>
----@field g table<string,string>
+--- @class options
+--- @field opt table<string, string>
+--- @field g table<string,string>
 
----Set neovim options
----@param options options neovim options table
+--- Set neovim options
+--- @param options options neovim options table
 M.set_options = function(options)
   for opt, variables in pairs(options) do
     for key, value in pairs(variables) do
@@ -14,14 +14,14 @@ M.set_options = function(options)
   end
 end
 
----@class mappings
----@field n table<string, string>
----@field v table<string, string>
----@field i table<string, string>
----@field x table<string, string>
+--- @class mappings
+--- @field n table<string, string>
+--- @field v table<string, string>
+--- @field i table<string, string>
+--- @field x table<string, string>
 
----Set neovim keymaps
----@param mappings mappings neovim keymaps table
+--- Set neovim keymaps
+--- @param mappings mappings neovim keymaps table
 M.set_mappings = function(mappings)
   for mode, maps in pairs(mappings) do
     for key, map in pairs(maps) do
@@ -31,24 +31,46 @@ M.set_mappings = function(mappings)
   end
 end
 
----Setup packer plugins
----@param plugins any plugin data
+-- --- Initialize autocmd
+-- --- @param autocmd table<string, table>
+-- M.set_autocmd = function(autocmd)
+--   local cmd = vim.api.nvim_create_autocmd
+
+--   for triggers, settings in pairs(autocmd) do
+--     cmd(triggers, settings)
+--   end
+-- end
+
+--- Setup packer plugins
+--- @param plugins any plugin data
 M.setup_plugins = function(plugins)
   vim.cmd [[packadd packer.nvim]]
 
-  require("packer").startup(function(use)
-    use "wbthomason/packer.nvim"
-    for plugin_id, plugin_data in pairs(plugins) do
-      --NVchad style ["plugin"] = {...}
-      if type(plugin_id) ~= "number" then
-        table.insert(plugin_data, 1, plugin_id)
-      end
-      use(plugin_data)
-    end
-  end)
-
-  --Run all config scripts
   local host = vim.g.vscode and "vscode" or "neovim"
+  local packer_config = require(host .. ".plugins.packer.config")
+  local packer_compiled_path = host .. ".plugins.packer.packer_compiled"
+  local packer_ok, _ = pcall(require, packer_compiled_path)
+
+  require("packer").startup({
+    function(use)
+      use { "wbthomason/packer.nvim", opt = true }
+      for plugin_id, plugin_data in pairs(plugins) do
+        -- NVchad style ["plugin"] = {...}
+        if type(plugin_id) ~= "number" then
+          table.insert(plugin_data, 1, plugin_id)
+        end
+        use(plugin_data)
+      end
+    end,
+
+    config = packer_config
+  })
+
+  if not packer_ok then
+    vim.cmd("PackerCompile")
+  end
+
+  -- Run all config scripts
   vim.cmd("runtime! lua/" .. host .. "/plugins/config/*.lua")
 end
 
---
+++ lua/vscode/plugins/packer/config.lua
@@ -0,0 +1,6 @@
+local config = {
+  -- Packer config
+  compile_path = vim.fn.stdpath("config") .. "/lua/vscode/plugins/packer/packer_compiled.lua"
+}
+
+return config
