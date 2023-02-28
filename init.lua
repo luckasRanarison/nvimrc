4aa974e24348443cda7dab23b38b80fae03e73c8
Luckas <luckasranarison@gmail.com>
2023-02-28 08:43:55 +0300

    refactor: host require

--- init.lua
+++ init.lua
@@ -1,7 +1,12 @@
-if vim.g.vscode then
-  -- VSCodeNeovim
-  require "vscode"
-else
-  -- Normal Neovim
-  require "neovim"
-end
+local host = vim.g.vscode and "vscode" or "neovim"
+
+local configurer = require "utils.configurer"
+local autocmd = require(host .. ".core.autocmd")
+local options = require(host .. ".core.options")
+local mappings = require(host .. ".core.mappings")
+local plugins = require(host .. ".plugins")
+
+configurer.set_autocmd(autocmd)
+configurer.set_options(options)
+configurer.set_mappings(mappings)
+configurer.setup_plugins(plugins)
