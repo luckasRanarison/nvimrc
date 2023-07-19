48e545571184e1042bde6a5d59f39c341b9f9de5
Luckas <luckasranarison@gmail.com>
2023-07-19 10:34:26 +0300

    remove: icelang utils

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -1,4 +1,4 @@
-local lspconfig = require("lsp.custom")
+local lspconfig = require("lspconfig")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
