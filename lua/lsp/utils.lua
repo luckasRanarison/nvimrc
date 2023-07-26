1b923a990b9f62767efe7cfeee9c6f9540d2b36d
Luckas <luckasranarison@gmail.com>
2023-07-26 17:28:52 +0300

    refactor: lsp utils

--- lua/lsp/utils.lua
+++ lua/lsp/utils.lua
@@ -27,30 +27,31 @@ M.set_keymaps = function(client, bufnr)
 end
 
 M.format_async = function(bufnr)
+  local handler = function(err, res, ctx)
+    if err then
+      local err_msg = type(err) == "string" and err or err.message
+      vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+      return
+    end
+
+    local is_loaded = vim.api.nvim_buf_is_loaded(bufnr)
+    local is_modified = vim.api.nvim_buf_get_option(bufnr, "modified")
+
+    if not is_loaded or is_modified then return end
+
+    if res then
+      local client = vim.lsp.get_client_by_id(ctx.client_id)
+      local offset_encoding = client and client.offset_encoding or "utf-16"
+      vim.lsp.util.apply_text_edits(res, bufnr, offset_encoding)
+      vim.api.nvim_buf_call(bufnr, function() vim.cmd("silent noautocmd update") end)
+    end
+  end
+
   vim.lsp.buf_request(
     bufnr,
     "textDocument/formatting",
     vim.lsp.util.make_formatting_params({}),
-    function(err, res, ctx)
-      if err then
-        local err_msg = type(err) == "string" and err or err.message
-        vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
-        return
-      end
-
-      if
-        not vim.api.nvim_buf_is_loaded(bufnr)
-        or vim.api.nvim_buf_get_option(bufnr, "modified")
-      then
-        return
-      end
-
-      if res then
-        local client = vim.lsp.get_client_by_id(ctx.client_id)
-        vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-        vim.api.nvim_buf_call(bufnr, function() vim.cmd("silent noautocmd update") end)
-      end
-    end
+    handler
   )
 end
 
