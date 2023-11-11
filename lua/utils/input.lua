fca239e9a23d6b0941d958f3f97e2063b0d1105d
Luckas <luckasranarison@gmail.com>
2023-11-11 18:19:22 +0300

    update: input & select

--- lua/utils/input.lua
+++ lua/utils/input.lua
@@ -1,23 +1,29 @@
 local M = {}
 
-M.indent = function()
-  local value = vim.fn.input({ prompt = "Set indentation: " })
-  local type = vim.bo[0].expandtab and "spaces" or "tabs"
-  local parsed = tonumber(value)
+local input = function(prompt, callback)
+  local value = vim.fn.input(prompt)
+  if value:len() ~= 0 then callback(value) end
+end
 
-  if parsed then
-    vim.bo[0].shiftwidth = parsed
-    vim.notify("Indentation set to " .. value .. " " .. type)
-  else
-    vim.notify("Invalid value", vim.log.levels.ERROR)
-  end
-  vim.notify("Indentation using ")
+M.indent = function()
+  input("Set indentation: ", function(value)
+    local type = vim.bo[0].expandtab and "spaces" or "tabs"
+    local parsed = tonumber(value)
+
+    if parsed then
+      vim.bo[0].shiftwidth = parsed
+      vim.notify("Indentation set to " .. value .. " " .. type)
+    else
+      vim.notify("Invalid value", vim.log.levels.ERROR)
+    end
+  end)
 end
 
 M.filetype = function()
-  local value = vim.fn.input({ prompt = "Set filetype: " })
-  vim.bo[0].filetype = value
-  vim.notify("Set filetype to " .. value)
+  input("Set filetype: ", function(value)
+    vim.bo[0].filetype = value
+    vim.notify("Filetype set to " .. value)
+  end)
 end
 
 return M
